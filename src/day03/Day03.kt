083e325c4cf116dfe7e4a1a8816f1624f3e98593
Robert Eschbach <robert.eschbach@iits-consulting.de>
2021-12-05 20:32:45 +0100

    day3 part 2

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -6,11 +6,11 @@ import readTestInputByDay
 
 fun main() {
 
-    fun getMostCommonBinary(s: String) = if (s.count { it == '0' } > s.count()/2 ) 0 else 1
+    fun getMostCommonBinary(s: String) = if (s.count { it == '0' } > s.count() / 2) 0 else 1
 
-    fun transformRowsToColumns(input: List<String>): MutableList<String> {
+    fun transformRowsToColumns(input: List<String>): List<String> {
         val amountOfLists = input.first().count()
-        val listOfColumns = MutableList(amountOfLists) {""}
+        val listOfColumns = MutableList(amountOfLists) { "" }
         input.forEach { s ->
             s.forEachIndexed { index, singleChar ->
                 listOfColumns[index] = listOfColumns[index].plus(singleChar)
@@ -40,13 +40,52 @@ fun main() {
         return gamma.toInt(2) * epsilon.toInt(2)
     }
 
+    fun whoHasTheLongest(
+        firstList: List<String>,
+        secondList: List<String>,
+        sizeDoesNotMatter: Boolean,
+    ): List<String> {
+        return if (sizeDoesNotMatter) {
+            if (firstList.count() < secondList.count()) firstList else secondList
+        } else if (firstList.count() >= secondList.count()) firstList else secondList
+    }
+
+    fun partitionByDigitAndReturnTheLongestList(
+        input: List<String>,
+        digit: Int,
+        sizeDoesNotMatter: Boolean,
+    ): List<String> {
+        val (startsWithOne, startsWithZero) = input.partition { it[digit] == '1' }
+        return whoHasTheLongest(startsWithOne, startsWithZero, sizeDoesNotMatter)
+    }
+
+    fun getOxygenOrCo2(input: List<String>, sizeDoesNotMatter: Boolean = true): Int {
+        var oxygen = input
+        var i = 0
+        while (oxygen.distinct().count() > 1) {
+            oxygen = partitionByDigitAndReturnTheLongestList(oxygen, i, sizeDoesNotMatter)
+            i++
+        }
+        return oxygen.first().toInt(2)
+    }
+
+    fun part2(input: List<String>): Int {
+        val oxygen = getOxygenOrCo2(input, false)
+        val co2 = getOxygenOrCo2(input)
+        return oxygen * co2
+    }
+
     val testInput = readTestInputByDay(3)
     Assertions.assertEquals(198, part1(testInput))
     Assertions.assertEquals(1, getMostCommonBinary("01110"))
     Assertions.assertEquals("10110", getGammaAsBinary(testInput))
     Assertions.assertEquals("01001", getEpsilonByGamma(getGammaAsBinary(testInput)))
 
+    Assertions.assertEquals(23, getOxygenOrCo2(testInput, false), "oxygen not correct")
+    Assertions.assertEquals(10, getOxygenOrCo2(testInput), "co2 not correct")
+
+
     val input = readInputByDay(3)
     println("part1: " + part1(input))
+    println("part2: " + part2(input))
 }
-
